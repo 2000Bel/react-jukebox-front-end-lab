d47eee441e3f1986b818335657093c49a7050c39
2000Bel <belvelizm@gmail.com>
2025-03-12 19:19:38 -0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+// src/App.jsx
+require ('dotenv').config();
+const apiKey
+
+const App = () => {
+  return <h1>Hello world!</h1>;
+};
+
+export default App;
+
---
+++ src/services/trackService.js
@@ -0,0 +1,1 @@
+ API_BASE_URL=http://api.weatherapi.com/v1/current.json?key=&q=London&aqi=no
