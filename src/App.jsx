14ed2cd8937bf60cf6c60d35be6961aed4638c1e
2000Bel <belvelizm@gmail.com>
2025-03-13 18:34:16 -0400

    init commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
-// src/App.jsx
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import TrackForm from "./components/TrackForm";
 
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import TrackForm from './components/TrackForm';
-
-const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/add-track" element={<TrackForm />} />
-        <Route path="/edit-track/:trackId" element={<TrackForm />} />
-      </Routes>
-    </Router>
-  );
-};
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/track-form" element={<TrackForm />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
--- src/services/trackService.js
+++ src/services/trackService.js
@@ -1,4 +1,3 @@
 
-const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/pets`;
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/tracks`;
 
- API_BASE_URL=http://api.weatherapi.com/v1/current.json?key=&q=London&aqi=no
