f45cca86d4f3bc8b4d75d591b8a15de70eecda61
2000Bel <belvelizm@gmail.com>
2025-03-12 21:42:15 -0400

    front

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 // src/App.jsx
-require ('dotenv').config();
-const apiKey
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import TrackForm from './components/TrackForm';
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add-track" element={<TrackForm />} />
+        <Route path="/edit-track/:trackId" element={<TrackForm />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
-
--- src/services/trackService.js
+++ src/services/trackService.js
@@ -1,1 +1,4 @@
+
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/pets`;
+
  API_BASE_URL=http://api.weatherapi.com/v1/current.json?key=&q=London&aqi=no
